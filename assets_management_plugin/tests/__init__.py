f3722faad384062e419d0996c9c60650c04bfa6d
Khoa Nguyen <nguyenkhoa14022002@gmail.com>
2026-05-12 14:07:30 +0700

    Remove Unnessary Files

--- assets_management_plugin/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Tests for NetBox assets-management Plugin.
-
-For more information on testing NetBox plugins, see:
-https://netboxlabs.com/docs/netbox/plugins/development/testing/
-"""
--- tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Unit test package for assets_management_plugin."""
